dfe9de81baea30d957a8de7584077b7a367ea92d
federicoimpellizzeri <f.impellizzeri1@studenti.uniba.it>
2019-06-07 11:59:50 +0200

    Grafici_personalizzabili

--- crypto_runner/prova_report_graph.py
+++ crypto_runner/prova_report_graph.py
@@ -2,6 +2,8 @@ import os
 
 # EXPERIMENT ONE
 # ------------------------------------------------------------------------------------------------------------------------
+from itertools import product
+
 import numpy
 import pandas as pd
 
@@ -105,16 +107,18 @@ from crypto_utility.report_data import report_configurations_SingleTarget, repor
 # dizionario = {"cryptostock_name": [], "date": [], "model": [], "neurons": [], "days": [],
 #               "real_value": [],
 #               "predicted_value": []}
-# stocks_data_path = "../crypto_preprocessing/step0_data/"
+stocks_data_path = "../crypto_preprocessing/step0_data/"
 #
 # # Lista nomi stocks
-# stocks_name = []
-# for name in os.listdir(stocks_data_path):
-#     stocks_name.append(name.replace(".csv", ""))
+stocks_name = []
+for name in os.listdir(stocks_data_path):
+    stocks_name.append(name.replace(".csv", ""))
 #
 # # Lista Cartelle esperimenti
-# exp_performed = ["SingleTarget_Data", "SingleTarget_Data_with_Indicators", "MultiTarget_Data",
-#                  "MultiTarget_Data_with_Indicators"]
+exp_performed = ["SingleTarget_Data", "SingleTarget_Data_with_Indicators", "MultiTarget_Data",
+                 "MultiTarget_Data_with_Indicators"]
+
+
 # # per ogni stock
 # #   per ogni cartella exp - Result
 # #       se ST:
@@ -146,7 +150,6 @@ from crypto_utility.report_data import report_configurations_SingleTarget, repor
 #                     "../" + e + "/Result/" + each_folder + "/" + each_conf + "/stats/predictions.csv")
 #                 for d, r, p in zip(predictions_file["date"], predictions_file["observed_norm"],
 #                                    predictions_file["predicted_norm"]):
-#                     x = 1
 #                     dizionario["cryptostock_name"].append(str(s))
 #                     dizionario["date"].append(d)
 #                     dizionario["model"].append(str(e))
@@ -167,7 +170,6 @@ from crypto_utility.report_data import report_configurations_SingleTarget, repor
 #                     "../" + e + "/Result/" + each_conf + "/stats/predictions.csv")
 #                 for d, r, p in zip(predictions_file["date"], predictions_file[str(s) + "_observed_norm"],
 #                                    predictions_file[str(s) + "_predicted_norm"]):
-#                     x = 1
 #                     dizionario["cryptostock_name"].append(str(s))
 #                     dizionario["date"].append(d)
 #                     dizionario["model"].append(str(e))
@@ -177,45 +179,81 @@ from crypto_utility.report_data import report_configurations_SingleTarget, repor
 #                     dizionario["predicted_value"].append(float(p))
 # pd.DataFrame(dizionario).to_csv("../FINAL/all.csv", index_label=False, index=False)
 
-data_final = pd.read_csv("../FINAL/all.csv")
-print(data_final.shape)
-btc = data_final[data_final["cryptostock_name"] == "BTC"]
-print("DOGE ->", btc.shape, "\t", type(btc))
-v2 = btc[btc["neurons"] == 128]
-print("V2-> ", v2.shape)
-
-l_crypto = ["BTC"]
-l_model = ["SingleTarget_Data"]
-l_nn = [128]
-l_dd = [30]
+# data_final = pd.read_csv("../FINAL/all.csv")
+# print(data_final.shape)
+# btc = data_final[data_final["cryptostock_name"] == "BTC"]
+# print("DOGE ->", btc.shape, "\t", type(btc))
+# v2 = btc[(btc["neurons"] == 128) & (btc["model"] == "SingleTarget_Data")]
+# print("V2-> ", v2.shape)
+# print(v2)
 
 
 def plot_graphs(input_data, list_crypto, list_model, list_neurons, list_days, output_path):
     data = pd.read_csv(input_data)
-    for crypto in list_crypto:
-        data_cut2 = data[data["cryptostock_name"] == crypto]
-        for m in list_model:
-            data_cut3 = data_cut2[data_cut2["model"] == m]
-            for nn in list_neurons:
-                data_cut4 = data_cut3[data_cut3["neurons"] == nn]
-                for dd in list_days:
-                    data_cut5 = data_cut4[data_cut4["days"] == dd]
-                    print(data_cut5)
-                    fig = plt.figure()
-                    line_chart_p = plt.plot(range(0, len(data_cut5["date"]), 1), data_cut5["predicted_value"])
-                    line_chart_r = plt.plot(range(0, len(data_cut5["date"]), 1), data_cut5["real_value"])
-                    plt.title('from 01/07/2016 to 26/06/2017')
-                    plt.xlabel('Test Date')
-                    plt.xticks(numpy.arange(12), data_cut5["date"], rotation=65)
-                    plt.yticks(numpy.arange(0.0, 1.0, 0.1))
-                    plt.ylabel('Value')
-                    plt.legend(['prediction', 'real'], loc=4)
-                    plt.grid()
-                    fig.tight_layout()
-                    # plt.show()
-                    name_fig = str(crypto) + "_" + str(m) + "_" + str(nn) + "_" + str(dd)
-                    fig.savefig(output_path + name_fig + ".png")
+
+    for name, neurons, days in product(list_crypto, list_neurons, list_days):
+        data_cutted = data[(data["cryptostock_name"] == name) & (data["neurons"] == neurons) & (data["days"] == days)]
+        fig = plt.figure(figsize=(12, 7))
+        ax = fig.add_subplot(111)
+        max_values = []
+        labels = []
+        for (m, i) in zip(list_model, range(0, len(list_model), 1)):
+            data_cutted_model_oriented = data_cutted[data["model"] == m]
+            if (i == 0):
+                ax.plot(range(0, len(data_cutted_model_oriented["date"]), 1),
+                        data_cutted_model_oriented["real_value"])
+                max_values.append(max(data_cutted_model_oriented["real_value"]))
+                labels.append("REAL")
+            ax.plot(range(0, len(data_cutted_model_oriented["date"]), 1),
+                    data_cutted_model_oriented["predicted_value"])
+            max_values.append(max(data_cutted_model_oriented["predicted_value"]))
+            labels.append("PREDICTED_" + str(m))
+        max_abs = max(max_values)
+        plt.title(str(name) + " - #Neurons:" + str(neurons) + " - Previous days:" + str(days))
+        plt.xticks(numpy.arange(12), data_cutted_model_oriented["date"], rotation=65)
+        plt.yticks(numpy.arange(0.0, max_abs, (max_abs / 100 * 5)))
+        plt.ylabel('Value')
+        plt.legend(labels, loc=4)
+        plt.grid()
+        fig.tight_layout()
+        name_fig = str(name) + "_" + str(neurons) + "_" + str(days)
+        fig.savefig(output_path + name_fig + ".png")
+
+    # for crypto in list_crypto:
+    #     data_cut2 = data[data["cryptostock_name"] == crypto]
+    #     for m in list_model:
+    #         data_cut3 = data_cut2[data_cut2["model"] == m]
+    #         for nn in list_neurons:
+    #             data_cut4 = data_cut3[data_cut3["neurons"] == nn]
+    #             for dd in list_days:
+    #                 data_cut5 = data_cut4[data_cut4["days"] == dd]
+    #                 # print(data_cut5)
+    #                 fig = plt.figure(figsize=(18, 12))
+    #                 line_chart_p = plt.plot(range(0, len(data_cut5["date"]), 1), data_cut5["predicted_value"])
+    #                 line_chart_r = plt.plot(range(0, len(data_cut5["date"]), 1), data_cut5["real_value"])
+    #                 plt.title(crypto + " " + str(m) + "\n" + "[" + str(nn) + "#Hidden Neurons \n" + str(
+    #                     dd) + "Previous Days considered]")
+    #                 plt.xlabel('Test Date')
+    #                 max1 = max(data_cut5["predicted_value"])
+    #                 max2 = max(data_cut5["real_value"])
+    #                 maximum = max(max1, max2)
+    #                 plt.xticks(numpy.arange(12), data_cut5["date"], rotation=65)
+    #                 plt.yticks(numpy.arange(0.0, maximum, 0.05))
+    #                 plt.ylabel('Value')
+    #                 plt.legend(['prediction', 'real'], loc=4)
+    #                 plt.grid()
+    #                 fig.tight_layout()
+    #                 # plt.show()
+    #                 name_fig = str(crypto) + "_" + str(m) + "_" + str(nn) + "_" + str(dd)
+    #                 fig.savefig(output_path + name_fig + ".png")
     return
 
 
-plot_graphs("../FINAL/all.csv", l_crypto, l_model, l_nn, l_dd, "../FINAL/")
+lista_single = ["SingleTarget_Data", "SingleTarget_Data_with_Indicators"]
+lista_multi = ["MultiTarget_Data", "MultiTarget_Data_with_Indicators"]
+lista_without = ["SingleTarget_Data", "MultiTarget_Data"]
+lista_with = ["SingleTarget_Data_with_Indicators", "MultiTarget_Data_with_Indicators"]
+l_crypto = ["BTC"]
+l_nn = [256]
+l_dd = [30, 200]
+plot_graphs("../FINAL/all.csv", l_crypto, lista_with, l_nn, l_dd, "../FINAL/")
